770b7b42a8530c0913085236386e4f5a146a8ab2
yuki <chenjf@mail.sustech.edu.cn>
2025-08-28 19:41:53 +0800

    init

---
+++ conf/proj.mk
@@ -0,0 +1,7 @@
+# DO NOT MODIFY THIS FILE
+
+## This File contains identifiers for your project submission.
+## This File should only be modified by the teaching team.
+
+PROJECT = 0
+HANDIN_SERVER = http://genshin.sqlab.cra.moe:8848/upload
