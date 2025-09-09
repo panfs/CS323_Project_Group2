7868b6ad3e6ec59a7d3db9b35eb71fea3cf7412e
Yuki Chen <chenjf@mail.sustech.edu.cn>
2025-09-09 18:27:30 +0800

    project1

---
+++ Splc.g4
@@ -0,0 +1,20 @@
+lexer grammar Splc;
+
+// IDEA Plugin Settings
+// - Output Directory: src/main/java/
+// - package name: generated.Splc
+
+// =========================
+// Lexer Rules
+// =========================
+
+// ---------- Keywords ----------
+INT     : 'int';
+
+// ---------- Operators ----------
+
+// ---------- Separators ----------
+
+// ---------- Identifiers & Literals ----------
+
+// ---------- Whitespace & Comments ----------
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,17 @@
+import framework.project1.Grader;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream("testcases/project1/case1.splc");
+        Grader grader1 = new Grader(fis);
+        grader1.run();
+
+        System.out.println("---");
+
+        Grader grader = new Grader("1 + 2");
+        grader.run();
     }
 }
