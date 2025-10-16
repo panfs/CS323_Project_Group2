92a7692506f4595831a731f0764b81b67d084640
panfs <12311222@mail.sustech.edu.cn>
2025-10-16 21:36:23 +0800

    Main synchronize

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,25 @@
+import framework.project2.Grader;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        {
+            InputStream input = new FileInputStream("testcases/project2/correct_testcase_1.splc");
+            Grader grader = new Grader(input, System.out, false);
+
+            grader.run();
+        }
+        System.out.println("\n--------------------\n");
+        {
+            InputStream input = new FileInputStream("testcases/project2/error_testcase_1.splc");
+            Grader grader = new Grader(input, System.out, true);
+
+            grader.run();
+        }
     }
 }
